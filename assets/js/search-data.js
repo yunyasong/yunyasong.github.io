0cfb636c73b7a7d1a47f34ea88aa6c565d545109
yunyasong <46914466+yunyasong@users.noreply.github.com>
2025-03-16 05:05:17 +0000

    Deploying to gh-pages from @ yunyasong/yunyasong.github.io@7cc88f18f591b34ac3a543f4065ec631625bf6c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "post-a-post-with-image-galleries",
+  },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
